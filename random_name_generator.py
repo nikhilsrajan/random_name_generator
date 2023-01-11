783b4bec119f4b32446e4e2dbaf055eef8edf3a2
Nikhil Sasi Rajan <nikhilsasirajan@gmail.com>
2023-01-11 15:33:29 +0530

    made filepaths relative

--- random_name_generator.py
+++ random_name_generator.py
@@ -1,11 +1,16 @@
 import sys
 import random
+import os
 
 
 def generate_random_name(sep='_'):
-    with open('nouns.txt', 'r') as h: nouns = [word.replace('\n', '') for word in h.readlines()]
-    with open('adjectives.txt', 'r') as h: adjectives = [word.replace('\n', '') for word in h.readlines()]
-    with open('verbs.txt', 'r') as h: verbs = [word.replace('\n', '') for word in h.readlines()]
+    package_dir = os.path.dirname(__file__)
+    nouns_filepath = os.path.join(package_dir, 'nouns.txt')
+    adjectives_filepath = os.path.join(package_dir, 'adjectives.txt')
+    verbs_filepath = os.path.join(package_dir, 'verbs.txt')
+    with open(nouns_filepath, 'r') as h: nouns = [word.replace('\n', '') for word in h.readlines()]
+    with open(adjectives_filepath, 'r') as h: adjectives = [word.replace('\n', '') for word in h.readlines()]
+    with open(verbs_filepath, 'r') as h: verbs = [word.replace('\n', '') for word in h.readlines()]
     random_name = f'{random.choice(verbs)}{sep}{random.choice(adjectives)}{sep}{random.choice(nouns)}'
     return random_name
 
